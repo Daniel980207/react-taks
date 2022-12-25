820c575e77a8b927b964fa832cdd55ebd36aabde
Daniel Acuña <eduardo11071@hotmail.com>
2022-12-24 22:09:41 -0500

    thrid commit

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -6,7 +6,7 @@ function TaskCard({ task }) {
   return (
     <div className="bg-gray-800 text-white p-4 rounded-md">
       <h1 className="text-x1 font-bold capitalize">Título : {task.title}</h1>
-      <p className="text-gray-500 text-5m">Descripción : {task.descripcion}</p>remote origin already exists.
+      <p className="text-gray-500 text-5m">Descripción : {task.descripcion}</p>
       <button
         className="bg-red-500 px-2 p2-1 rounded-md mt-4 hover:bg-red-400"
         onClick={() => deleteTask(task.id)}
